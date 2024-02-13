953b575d415b096a7762d015698c21221a259d78
Shivam Chouhan <shivamc86@gmail.com>
2024-02-13 21:26:23 +0530

    Created new animations, improved the card changer function

--- scripts.js
+++ scripts.js
@@ -5,32 +5,48 @@ document.addEventListener('DOMContentLoaded', function() {
     var message = ["Hiii... Namaste ji<br> Kaisi Ho<br> I hope sab theek hi hai",
                     "I know kal se exams hain<br>Tu shaayad padh hi rahi hogi<br>isiliye ZYAADA time nahi loonga",
                     "!!! Ve yaar !!!<br> teri yaad hi itni aa rahi thi<br>Ki fir mujhse raha nahi gaya",
-                    "Tujhe ITNE message karta hoon<br>ye soch ke ki <br>KABHI TO DEKHEGI<br>But got disappointed there as well",
+                    "Tujhe ITNE message karta hoon<br>ye soch ke ki <br>KABHI  TO  DEKHEGI<br>But got disappointed there as well",
                     "Khair koina<br>Kal bhi din bhar man mein tera hi zikr chal raha tha<br>Fir shaam ko ek khayaal aaya<br>Ki priye aap",
                     "Gulaab Jaisi ho<br>Gulaab lagti ho<br>Halka sa bhi muskura do<br>Ammi kasam Lajawab lagti ho",
                     "Aur bhi bohot kuch kehna hai tumse<br>Khair chchodiye, kabhi aur kahenge",
-                    "Padhaai-Revision to ab ho hi gaya hoga<br>Practicals bhi, I assume achche hi gaye honge<br>tu achchi bachchi hai",
-                    "Aur priye<br>Khaana time par khaiyega<br>aur Tabiyat ka bhi lihaaz rakhiyega<br>zyaada load mat lena, chill kar",
-                    "Dahi shakkar kha kar jaana ghar se<br>Baaki Shiv ji se roz tere liye prarthna to main karta hoon hi",
+                    "Padhaai-Revision to sab ho hi gaya hoga<br>Practicals bhi, I assume achche hi gaye honge<br>tu achchi bachchi hai<br>Achche marks aayenge",
+                    "Aur priye<br>Khaana time par kha lena<br>aur Tabiyat ka bhi lihaaz rakhna<br>zyaada load mat lena, chill kar",
+                    "Dahi shakkar kha kar jaana ghar se<br>Baaki Shiv ji se roz tere liye prarthna to main karta hoon hi<br>phod dena paper ko ekdam",
                     "BYE<br>BEST OF LUCK FOR YOUR BOARDS<br>aur haan<br>saath mein glucose ya fir candies rakhna<br>sugar intake brain ko active rakhta hai<br>Bye LOVE u"];
 
-    var imageSet = ["./resources/Stiker 10 (SFILE.MOBI).gif",
-                    "./resources/Stiker 7 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 8 (SFILE.MOBI).gif",
-                    "./resources/Stiker 13 (SFILE.MOBI).gif"];
+    var imageSet = ["./resources/gifs/Stiker 16 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 38 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 49 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 30 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 10 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 26 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 7 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 31 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 27 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 13 (SFILE.MOBI).gif",
+                    "./resources/gifs/Stiker 33 (SFILE.MOBI).gif"];
 
     const cardImg = document.querySelector('.card-img img');
     const cardText = document.querySelector('.card-text');
 
+    const card = document.querySelector('.card');
+
+function triggerCardAnimationleft() {
+    card.classList.add('animate-cardleft');
+
+    setTimeout(() => {
+        card.classList.remove('animate-cardleft');
+    }, 500);
+}
+function triggerCardAnimationright() {
+    card.classList.add('animate-cardright');
+
+    setTimeout(() => {
+        card.classList.remove('animate-cardright');
+    }, 500);
+}
+
     function displayCard(index) {
-        console.log(message.length, message[index], index);
         if (index === 0) {
             prevButton.style.display = "none";
             nextButton.style.display = "flex";
@@ -40,7 +56,6 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             prevButton.style.display = "flex";
             nextButton.style.display = "flex";
-            console.log("Did nothing!!");
         }
 
         cardImg.src = imageSet[index];
@@ -49,13 +64,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     nextButton.addEventListener('click', function() {
         currentIndex = (currentIndex + 1) % message.length;
+        triggerCardAnimationleft();
         displayCard(currentIndex);
     });
 
     prevButton.addEventListener('click', function() {
         currentIndex = (currentIndex - 1 + message.length) % message.length;
+        triggerCardAnimationright();
         displayCard(currentIndex);
     });
 
+
+
+    triggerCardAnimationleft();
     displayCard(currentIndex);
 });
