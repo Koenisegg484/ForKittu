f110bf1a9945f2e910f77bf8f27691618b928838
Shivam Chouhan <shivamc86@gmail.com>
2024-02-13 17:43:35 +0530

    Re did the css part 2, added newer fonts

--- scripts.js
+++ scripts.js
@@ -5,10 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var message = ["Hiii... Namaste ji<br> Kaisi Ho<br> I hope sab theek hi hai",
                     "I know kal se exams hain<br>Tu shaayad padh hi rahi hogi<br>isiliye ZYAADA time nahi loonga",
                     "!!! Ve yaar !!!<br> teri yaad hi itni aa rahi thi<br>Ki fir mujhse raha nahi gaya",
-                    "Tujhe ITNE message karta hoon<br>ye soch ke ki <br>KABHI TO DEKHEGI<br>But got disappointed there as well"];
+                    "Tujhe ITNE message karta hoon<br>ye soch ke ki <br>KABHI TO DEKHEGI<br>But got disappointed there as well",
+                    "Khair koina<br>Kal bhi din bhar man mein tera hi zikr chal raha tha<br>Fir shaam ko ek khayaal aaya<br>Ki priye aap",
+                    "Gulaab Jaisi ho<br>Gulaab lagti ho<br>Halka sa bhi muskura do<br>Ammi kasam Lajawab lagti ho",
+                    "Aur bhi bohot kuch kehna hai tumse<br>Khair chchodiye, kabhi aur kahenge",
+                    "Padhaai-Revision to ab ho hi gaya hoga<br>Practicals bhi, I assume achche hi gaye honge<br>tu achchi bachchi hai",
+                    "Aur priye<br>Khaana time par khaiyega<br>aur Tabiyat ka bhi lihaaz rakhiyega<br>zyaada load mat lena, chill kar",
+                    "Dahi shakkar kha kar jaana ghar se<br>Baaki Shiv ji se roz tere liye prarthna to main karta hoon hi",
+                    "BYE<br>BEST OF LUCK FOR YOUR BOARDS<br>aur haan<br>saath mein glucose ya fir candies rakhna<br>sugar intake brain ko active rakhta hai<br>Bye LOVE u"];
 
     var imageSet = ["./resources/Stiker 10 (SFILE.MOBI).gif",
                     "./resources/Stiker 7 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
+                    "./resources/Stiker 8 (SFILE.MOBI).gif",
                     "./resources/Stiker 8 (SFILE.MOBI).gif",
                     "./resources/Stiker 13 (SFILE.MOBI).gif"];
 
